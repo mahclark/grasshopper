3860afdd156caeeae02c57c39a2e38560ddebeb8
wsc34 <winson.2000.l.h.75@gmail.com>
2019-05-19 17:39:12 +0100

    update

---
+++ src/fx/EventEditView.java
@@ -0,0 +1,18 @@
+package fx;
+
+import backend.Location;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import weather.LocationWeatherOWM;
+
+import java.util.List;
+
+public class EventEditView extends View {
+    private Stage stage;
+    private Scene scene;
+
+    private Pane root = new Pane();
+}
