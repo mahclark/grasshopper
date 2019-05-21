3960acbb9807b870acb6f422a7823d7cad56e952
lbc34 <50453701+lbc34@users.noreply.github.com>
2019-05-21 14:45:17 +0100

    Prepare for JAR build

---
+++ src/launcher/Launcher.java
@@ -0,0 +1,10 @@
+package launcher;
+
+import fx.Main;
+
+//required to get round Java 9 modularity problems
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
