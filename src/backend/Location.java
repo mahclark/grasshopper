6d43830cc19b9759ed5cbbbc10ac15274d8b8760
jwow5231 <49236832+jwow5231@users.noreply.github.com>
2019-05-21 14:46:33 +0100

    time delay

--- src/backend/Location.java
+++ src/backend/Location.java
@@ -11,6 +11,8 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class Location {
 
@@ -36,6 +38,7 @@ public class Location {
     }
 
     public Location (String input) {
+
         this.input = input;
 
         this.GeocodeSearchUrl = String.format(GEOCODE_BASE_URL, GEOCODE_TOKEN, input);
@@ -44,7 +47,9 @@ public class Location {
 
             URL url = new URL(GeocodeSearchUrl);
             URLConnection request = url.openConnection();
+            TimeUnit.MILLISECONDS.sleep(1000);
             request.connect();
+
             JsonParser jp = new JsonParser(); //from gson
             JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
             JsonArray rootArr = root.getAsJsonArray(); // convert json element to json array as the output gives an array of json objects
@@ -56,7 +61,7 @@ public class Location {
 
         }
 
-        catch (IOException e) {
+        catch (Exception e) {
             System.err.println("IOException: "+e.getMessage());
             this.lat = 52.2107375;
             this.lon = 0.09179849999999999;
